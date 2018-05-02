c188cfcef9d2bc5f52a5508e0169e079d41b33e0
Mirela <mirela.kulenovic@live.se>
2018-05-02 11:20:59 +0200

    Browser View created. MovieGridPartial added. css fix.

---
+++ Pop.ly/Controllers/BrowseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pop.ly.Models.Database;
+using Pop.ly.Models;
+
+namespace Pop.ly.Controllers
+{
+    public class BrowseController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();       
+        // GET: Browse
+        public ActionResult Index()
+        {
+            BrowseModel model = new BrowseModel();
+            model.movies = db.Movies.Select(m => m).ToList(); 
+            return View(model);
+        }
+        
+    }
+}
