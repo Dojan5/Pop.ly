9d46a7116aa80fe84b21c5841611a80b4b1ebcb8
Mirela <mirela.kulenovic@live.se>
2018-05-02 14:54:32 +0200

    css, updated style

--- Pop.ly/Scripts/Site.js
+++ Pop.ly/Scripts/Site.js
@@ -1,3 +1,27 @@
 ﻿$(document).ready(function () {
     
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function toggleNav() {
+    if (document.getElementById("toggleSideNav").value == "O")
+    {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("toggleSideNav").value = "C";
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("toggleSideNav").value = "O";
+    }
+}
