67b2da7fd60d38939315d3d75d1175032329549d
Candy <mirela.kulenovic@live.se>
2018-05-03 14:50:40 +0200

    Text Browse Controller

--- Pop.ly/Controllers/BrowseController.cs
+++ Pop.ly/Controllers/BrowseController.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Pop.ly.Models.Database;
+using Pop.ly.Models;
 
 namespace Pop.ly.Controllers
 {
     public class BrowseController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Browse
         public ActionResult Index()
         {
+
             return View();
         }
-        public ActionResult Year(int y)
+        public ActionResult Year(int Year)
         {
-            var Year = new Year();
-            var dbm = db.Movies.Where(m => m.ReleaseYear == Year).Select(m => m);
-            Movie = dbm.First();
-            return View(Movie);
+            
+            
+            var model = db.Movies.Where(m => m.ReleaseYear == Year).Select(m => m);
+            
+            return View(model);
             
          
         }
